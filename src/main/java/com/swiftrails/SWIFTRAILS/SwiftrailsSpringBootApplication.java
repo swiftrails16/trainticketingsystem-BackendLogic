dc9508f2a308ff93760dc48fa865b1f2a44077ad
monilmody9 <162667348+monilmody9@users.noreply.github.com>
2024-04-09 00:19:08 -0400

    Update SwiftrailsSpringBootApplication.java

--- src/main/java/com/swiftrails/SWIFTRAILS/SwiftrailsSpringBootApplication.java
+++ src/main/java/com/swiftrails/SWIFTRAILS/SwiftrailsSpringBootApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 @EnableAutoConfiguration
 public class SwiftrailsSpringBootApplication {
+
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SwiftrailsSpringBootApplication.class, args);
