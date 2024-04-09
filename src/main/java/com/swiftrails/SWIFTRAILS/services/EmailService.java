85682720942da29d0bf38f9949296ec34b90712d
swiftrails16 <162662006+swiftrails16@users.noreply.github.com>
2024-04-08 21:37:00 -0400

    Update EmailService.java

--- src/main/java/com/swiftrails/SWIFTRAILS/services/EmailService.java
+++ src/main/java/com/swiftrails/SWIFTRAILS/services/EmailService.java
@@ -4,5 +4,5 @@ import java.util.concurrent.CompletableFuture;
 
 public interface EmailService {
 	public CompletableFuture<Void> sendEmail(String to, String subject, String text) throws Exception;
-    public String getOtpLoginEmailTemplate(String name, String otp) ;
+    public String getOtpLoginEmailTemplate(String name, String otp) ; 
 }
